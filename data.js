147a4e4d76cde3de2bfc949888a2f97306f081cf
mark56314 <105619901+mark56314@users.noreply.github.com>
2026-01-23 03:43:02 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+// Здесь хранятся все рестораны
+let restaurants = [
+  {name:"Ресторан 1", phone:"+79991234567", tg:"https://t.me/...", map:"https://2gis.ru/..."},
+  {name:"Ресторан 2", phone:"+79997654321", tg:"https://t.me/...", map:"https://2gis.ru/..."}
+];
