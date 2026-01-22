c036794cbf496365c8285aeac1b655541f41bc6b
mark56314 <105619901+mark56314@users.noreply.github.com>
2026-01-23 03:55:18 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,13 +1,33 @@
+const loginDiv = document.getElementById("loginDiv");
+const adminDiv = document.getElementById("adminDiv");
+const passwordInput = document.getElementById("passwordInput");
+const loginBtn = document.getElementById("loginBtn");
+
 const listDiv = document.getElementById("restaurantList");
 const nameInp = document.getElementById("name");
 const phoneInp = document.getElementById("phone");
 const tgInp = document.getElementById("tg");
 const mapInp = document.getElementById("map");
 const saveBtn = document.getElementById("saveBtn");
+const logoUpload = document.getElementById("logoUpload");
 
 let editIndex = -1;
 
-function renderList() {
+// ПРОСТОЙ ПАРОЛЬ
+const adminPassword = "1234";
+
+loginBtn.onclick = () => {
+  if(passwordInput.value === adminPassword){
+    loginDiv.style.display = "none";
+    adminDiv.style.display = "block";
+    renderList();
+  }else{
+    alert("Неверный пароль");
+  }
+}
+
+// CRUD рестораны
+function renderList(){
   listDiv.innerHTML = "";
   restaurants.forEach((r,i)=>{
     const div = document.createElement("div");
@@ -19,8 +39,6 @@ function renderList() {
     listDiv.appendChild(div);
   });
 }
-renderList();
-
 window.edit = (i)=>{
   editIndex=i;
   const r = restaurants[i];
@@ -29,7 +47,6 @@ window.edit = (i)=>{
   tgInp.value=r.tg;
   mapInp.value=r.map;
 }
-
 window.remove = (i)=>{
   restaurants.splice(i,1);
   renderList();
@@ -37,10 +54,10 @@ window.remove = (i)=>{
 
 saveBtn.onclick = ()=>{
   const newData = {
-    name: nameInp.value,
-    phone: phoneInp.value,
-    tg: tgInp.value,
-    map: mapInp.value
+    name:nameInp.value,
+    phone:phoneInp.value,
+    tg:tgInp.value,
+    map:mapInp.value
   };
   if(editIndex>=0){
     restaurants[editIndex]=newData;
@@ -50,5 +67,16 @@ saveBtn.onclick = ()=>{
   editIndex=-1;
   nameInp.value=phoneInp.value=tgInp.value=mapInp.value="";
   renderList();
-  alert("Сохранено (данные живут в этом браузере пока не обновить репозиторий)");
+  alert("Сохранено!");
+}
+
+// Загрузка логотипа
+logoUpload.onchange = function(e){
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onload = function(ev){
+    logoSrc = ev.target.result;
+    alert("Логотип обновлен!");
+  }
+  reader.readAsDataURL(file);
 }
